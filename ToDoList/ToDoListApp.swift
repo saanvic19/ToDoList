4a21344f9e2adbcce24a58dd1cbd531578b76f40
Saanvi <chsaanvi@gmail.com>
2025-07-28 11:57:02 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Saanvi Chiluveru on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
